e94a853a95f78f33d7ce088cd4d824e221e2d209
Horia Apostol <apostol_dan1901@yahoo.com>
2024-09-08 17:08:58 +0300

    fixes

--- vanadio-storefront/src/app/app.module.ts
+++ vanadio-storefront/src/app/app.module.ts
@@ -8,14 +8,14 @@ import { HomeComponent } from './components/home/home.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { EventsComponent } from './components/events/events.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HeaderMenuComponent } from './components/header/header-menu/header-menu.component';
+import {HeaderMenuComponent} from "./components/header/header-menu/header-menu.component";
 import { ColorBannerComponent } from './components/home/color-banner/color-banner.component';
 import { LogoCardComponent } from './components/home/logo-card/logo-card.component';
 import { SocialsComponent } from './components/socials/socials.component';
 import { PresentationComponent } from './components/home/presentation/presentation.component';
 import { ContactRegistrationComponent } from './components/contact-us/contact-registration/contact-registration.component';
 import { ContactBannerComponent } from './components/contact-us/contact-banner/contact-banner.component';
-import { HamburgerComponent } from './components/hamburger/hamburger.component';
+import { HamburgerComponent } from './components/header/hamburger/hamburger.component';
 
 @NgModule({
   declarations: [
